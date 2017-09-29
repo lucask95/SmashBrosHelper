3983bfd98f09d7f9f716146e23ce25542c87e321
Unknown <ljkeller@ucdavis.edu>
2017-09-28 22:39:10 -0700

    Implemented variable number of inputs

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
 function drawInputs() {
     var nPlayers = parseInt($("#numPlayers").val());
-    var nColumns = nPlayers % 5;
+    var nColumns = nPlayers / 5;
 
-    $("inputForm").html("");
+    $("#inputForm").html("");
 
     for (var i = 0; i < nColumns; i++) {
         var colText = "<div class=\"col\" id=\"inputCol" + i + "\"></div>";
-        $("inputForm").append("");
+        $("#inputForm").append(colText);
     }
 
     for (var i = 0; i < nPlayers; i++) {
-        var col = Math.floor(i % 5);
-        var labelText = "<label for=\"player" + (i + 1) + "\">" + (i + 1) + "</label>\n";
-        var inputText = "<input type=\"text\" id=\"player" + (i + 1) + "\">";
+        console.log("player " + (i + 1));
+        var col = Math.floor(i / 5);
+        var inputText = "<input type=\"text\" id=\"player" + (i + 1) +
+            "\" placeholder=\"Rank #" + (i + 1) + "\" />";
+        $("#inputCol" + col).append(inputText);
     }
 }
 
 $(document).ready(function() {
-
+    drawInputs();
+    $("#numPlayers").on("change", drawInputs);
 });
