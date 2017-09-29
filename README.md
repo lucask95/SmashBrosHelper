e7d865e3243d3872aadc1c1e22ac437ffd14809f
Unknown <ljkeller@ucdavis.edu>
2017-09-28 23:15:58 -0700

    Started text draw implementation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#SmashBrosHelper
+# SmashBrosHelper
+
+Tool intended to generate PR images for Smash Bros Facebook groups to use as cover photos.
--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var canvas;
+var ctx;
+
 function drawInputs() {
     var nPlayers = parseInt($("#numPlayers").val());
     var nColumns = nPlayers / 5;
@@ -10,7 +13,6 @@ function drawInputs() {
     }
 
     for (var i = 0; i < nPlayers; i++) {
-        console.log("player " + (i + 1));
         var col = Math.floor(i / 5);
         var inputText = "<input type=\"text\" id=\"player" + (i + 1) +
             "\" placeholder=\"Rank #" + (i + 1) + "\" />";
@@ -18,7 +20,29 @@ function drawInputs() {
     }
 }
 
+function initCanvas() {
+    canvas = document.getElementById('prCanvas');
+    ctx = canvas.getContext('2d');
+    ctx.fillStyle = "#e3e3e3";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function generateImage() {
+    var nPlayers = parseInt($("#numPlayers").val());
+    var tags = [];
+
+    for (var i = 0; i < nPlayers; i++) {
+        var tag = $("#player" + (i + 1)).val();
+        ctx.font = "1rem Roboto";
+        ctx.fillStyle = "#000";
+        ctx.fillText(tag, 10, i * 10 + 10 + (i * 20));
+    }
+
+}
+
 $(document).ready(function() {
     drawInputs();
     $("#numPlayers").on("change", drawInputs);
+    initCanvas();
+    $("#goBtn").on("click", generateImage);
 });
